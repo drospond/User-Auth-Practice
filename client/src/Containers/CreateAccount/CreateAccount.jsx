ff9c8e601e64ebfd02157c4ae31e6f468bd85160
drospond <david.rospond@gmail.com>
2020-06-03 17:45:42 -0400

    create account page

---
+++ client/src/Containers/CreateAccount/CreateAccount.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class CreateAccount extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Sign In</h1>
+                <form>
+                    <div className="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control"aria-describedby="emailHelp" />
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleInputEmail1">Username</label>
+                        <input type="email" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <label for="inputPassword1">Password</label>
+                        <input type="password" className="form-control" id="inputPassword1" />
+                    </div>
+                    <div className="form-group">
+                        <label for="inputPassword2">Repeat Password</label>
+                        <input type="password" className="form-control" id="inputPassword2" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default CreateAccount;
--- client/src/Containers/SignIn/SignIn.jsx
+++ client/src/Containers/SignIn/SignIn.jsx
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class SignIn extends Component {
+    
+
     render() {
         return (
             <div className="container">
                 <h1>Sign In</h1>
                 <form>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
         );
