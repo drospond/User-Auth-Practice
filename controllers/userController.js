e5afda5dd44c709e9e7001d86d6d79052409579f
drospond <david.rospond@gmail.com>
2020-06-03 15:20:55 -0400

    created user controller

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+
+router.post("/", (req, res) => {
+    const email = req.body.email.trim();
+    const password = req.body.email.trim();
+    const username = req.body.usernam.trim();
+
+    db.User.create(req.body).then(() => res.json({ succes: true })).catch(er => { console.log(er) })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const db = require("./models");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,10 +20,17 @@ app.get("/api/config", (req, res) => {
 
 app.use(express.static("client/build"));
 
+
+const UserController = require("./controllers/userController")
+app.use("/api/user", UserController);
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Express App is running on http://localhost:${PORT}`);
-});
+// db.sequelize.sync({force: true}).then(function () {
+  db.sequelize.sync().then(function () {
+    app.listen(PORT, () => {
+      console.log(`App is running on http://localhost:${PORT}`);
+    });
+  });
