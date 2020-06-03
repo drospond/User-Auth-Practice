849d644c49c0e1fa53e0403fe32c3fed96715639
drospond <david.rospond@gmail.com>
2020-06-03 17:25:44 -0400

    post user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ const db = require("../models");
 router.post("/", (req, res) => {
     const email = req.body.email.trim();
     const password = req.body.email.trim();
-    const username = req.body.usernam.trim();
+    const username = req.body.username.trim();
 
     db.User.create(req.body).then(() => res.json({ succes: true })).catch(er => { console.log(er) })
 })
