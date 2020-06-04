a060f4904537d512c600077f7ce0c3caeb547252
drospond <david.rospond@gmail.com>
2020-06-04 19:53:39 -0400

    state and change listeners added

--- client/src/Containers/SignIn/SignIn.jsx
+++ client/src/Containers/SignIn/SignIn.jsx
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { Link } from "react-router-dom";
 
+
+
 class SignIn extends Component {
+  state={
+    email: "",
+    password:"",
+    error: ""
+  }
+
+  handleInputChange =(event) =>{
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value,
+      error: ""
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <h1>Sign In</h1>
         <form>
           <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label for="email">Email address</label>
             <input
               type="email"
               className="form-control"
-              id="exampleInputEmail1"
+              id="email"
               aria-describedby="emailHelp"
+              name="email"
+              onChange={this.handleInputChange}
             />
             <small id="emailHelp" className="form-text text-muted">
               We'll never share your email with anyone else.
@@ -26,6 +44,8 @@ class SignIn extends Component {
               type="password"
               className="form-control"
               id="exampleInputPassword1"
+              name="password"
+              onChange={this.handleInputChange}
             />
           </div>
           <button type="submit" className="btn btn-primary">
