ec63c73823b689fce514edfce36bca18d217238e
drospond <david.rospond@gmail.com>
2020-06-04 21:52:33 -0400

    state and handleInputChange added

--- client/src/Containers/CreateAccount/CreateAccount.jsx
+++ client/src/Containers/CreateAccount/CreateAccount.jsx
@@ -1,33 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CreateAccount extends Component {
-    render() {
-        return (
-            <div className="container">
-                <h1>Create Account</h1>
-                <form>
-                    <div className="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control"aria-describedby="emailHelp" />
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div className="form-group">
-                        <label for="exampleInputEmail1">Username</label>
-                        <input type="email" className="form-control"/>
-                    </div>
-                    <div className="form-group">
-                        <label for="inputPassword1">Password</label>
-                        <input type="password" className="form-control" id="inputPassword1" />
-                    </div>
-                    <div className="form-group">
-                        <label for="inputPassword2">Repeat Password</label>
-                        <input type="password" className="form-control" id="inputPassword2" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-            </div>
-        );
-    }
+  state = {
+    email: "",
+    username: "",
+    password: "",
+    passwordRepeat: "",
+    error: "",
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+      error: "",
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Create Account</h1>
+        <form>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              aria-describedby="emailHelp"
+              onChange={this.handleInputChange}
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Username</label>
+            <input type="email" className="form-control" name="username" onChange={this.handleInputChange}/>
+          </div>
+          <div className="form-group">
+            <label for="inputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword1"
+              name="password"
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label for="inputPassword2">Repeat Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword2"
+              name="passwordRepeat"
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CreateAccount;
