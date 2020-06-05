8f5a688057878c9a1049807c6d372a92cfedde5a
drospond <david.rospond@gmail.com>
2020-06-04 21:30:34 -0400

    bcrypt to hash password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+const bcrypt = require("bcrypt");
 
-router.post("/", (req, res) => {
-  const email = req.body.email.trim();
-  const password = req.body.email.trim();
-  const username = req.body.username.trim();
+router.post("/", async (req, res) => {
+  try{
+    const email = req.body.email.trim();
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const username = req.body.username.trim();
 
-  db.User.create(req.body)
-    .then(() => res.json({ succes: true }))
-    .catch((er) => {
-      console.log(er);
-      res.status(500).json({succes:false});
-    });
+    db.User.create({email, password: hashedPassword, username})
+      .then(() => res.status(201).json({ succes: true }))
+      .catch((er) => {
+        console.log(er);
+        res.status(500).json({succes:false});
+      });
+  } catch{
+    res.status(500).send();
+  }
 });
 
 router.post("/signin", (req, res) => {
